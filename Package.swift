0f7d5e9615ddfb40745e1f2fabd3c448bb43b7b3
Bitmovin Release Automation <support@bitmovin.com>
2023-03-01 13:21:40 +0100

    Release 3.35.1-a.104

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.35.1-a.103/BitmovinPlayer.zip",
-            checksum: "0186c62fb848a65cdd7dae742b08fa54753ea64e6ee7b5df27fbd9f1c85739ba"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.35.1-a.104/BitmovinPlayer.zip",
+            checksum: "0e2608128113808f291316579c17343b8687836720cace3ebbd27d636b86ef71"
         )
     ]
 )
