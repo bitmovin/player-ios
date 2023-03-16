41efa4db03e5bca35d35c341c6c42eedaf5d02cf
Bitmovin Release Automation <support@bitmovin.com>
2023-03-16 17:07:02 +0100

    Release 3.36.0-rc.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.36.0-a.8/BitmovinPlayer.zip",
-            checksum: "3b61965451ca4f9d08ffe2161b51422dfe32bf448528f18aaae5795af8b4aceb"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.36.0-rc.1/BitmovinPlayer.zip",
+            checksum: "997ea5f31dfd0c7fd6b2e35a3f9ad6eaae371acfb2ac5af311b6a446730ccbb4"
         )
     ]
 )
