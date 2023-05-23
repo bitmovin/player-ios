afef31180bdb7763af9f618b125566417f11359d
Bitmovin Release Automation <support@bitmovin.com>
2023-05-23 14:02:04 +0200

    Release 3.39.0-a.28

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,14 +12,32 @@ let package = Package(
     products: [
         .library(
             name: "BitmovinPlayer",
-            targets: ["BitmovinPlayer"]
+            targets: ["BitmovinPlayerTarget"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "3.39.0-a.28"),
+        .package(url: "https://github.com/bitmovin/bitmovin-analytics-collector-ios", from: "2.11.1-a.95"),
     ],
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.39.0-b.3/BitmovinPlayer.zip",
-            checksum: "cc51970179cea7c5768737cceb596cfc94d5bc9340094a5f6b0992fd74f95c7a"
-        )
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.39.0-a.28/BitmovinPlayer.zip",
+            checksum: "57c24807e9506b207c8eb038c44e6edeb486118aaac3fcc58080d3e639eb7b24"
+        ),
+        .binaryTarget(
+            name: "BitmovinPlayerAnalytics",
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.39.0-a.28/BitmovinPlayerAnalytics.zip",
+            checksum: "de8113b8239584bd8abe99a394cb3734f236ddb13af62ea009ea103583067dbb"
+        ),
+        .target(
+            name: "BitmovinPlayerTarget",
+            dependencies: [
+                "BitmovinPlayer",
+                "BitmovinPlayerAnalytics",
+                .product(name: "BitmovinCollector", package: "bitmovin-analytics-collector-ios"),
+                .product(name: "BitmovinPlayerCore", package: "player-ios-core"),
+            ]
+        ),
     ]
 )
