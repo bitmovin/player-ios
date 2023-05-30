df7e28f2fd76225f0c50ffddb21349c25be3aae8
Bitmovin Release Automation <support@bitmovin.com>
2023-05-30 15:25:02 +0200

    Release 3.39.0-a.29

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,14 +12,24 @@ let package = Package(
     products: [
         .library(
             name: "BitmovinPlayer",
-            targets: ["BitmovinPlayer"]
+            targets: ["BitmovinPlayerTarget"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "x.x.x")
     ],
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.39.0/BitmovinPlayer.zip",
-            checksum: "e4017d5066d260bef0ec6365b43a808c1d254ffa9b2873f9ba5b13ed35130333"
-        )
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.39.0-a.29/BitmovinPlayer.zip",
+            checksum: "62423b202cc38718377895f8b3c650befa021328a5652e7e23512001fc523884"
+        ),
+        .target(
+            name: "BitmovinPlayerTarget",
+            dependencies: [
+                "BitmovinPlayer",
+                .product(name: "BitmovinPlayerCore", package: "player-ios-core"),
+            ]
+        ),
     ]
 )
