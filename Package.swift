be093bae2654e55b196889bb2ca645cea63eb11b
Bitmovin Release Automation <support@bitmovin.com>
2023-07-17 16:00:00 +0200

    Release 3.41.1

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "3.41.1-rc.1"),
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "3.41.1"),
     ],
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.41.1-rc.1/BitmovinPlayer.zip",
-            checksum: "2824b38c81266543cde528f07247e55ccc309a20dcf342ce18bac50add333320"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.41.1/BitmovinPlayer.zip",
+            checksum: "82c27a9f6be29e6ef926c2c2f9ba92823838dae9e83a92a37284f84cbe34046f"
         ),
         .target(
             name: "BitmovinPlayerTarget",
