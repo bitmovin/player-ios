1f91e227344f765587057ac6432f2b66f2bd6e62
Bitmovin Release Automation <player-sdks@bitmovin.com>
2025-03-28 08:51:38 +0100

    Release 3.86.1-a.1

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -16,19 +16,19 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "3.86.0"),
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", "3.86.1-a.1"..."3.86.1"),
         .package(url: "https://github.com/bitmovin/bitmovin-analytics-collector-ios.git", from: "3.8.0"),
     ],
     targets: [
         .binaryTarget(
             name: "BitmovinPlayer",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.86.0/BitmovinPlayer.zip",
-            checksum: "1b6b5cbbe63ab21b32d12f1647b395e9f5cf1d584f11c57efb8cffeace30309c"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.86.1-a.1/BitmovinPlayer.zip",
+            checksum: "b8997104c3b4e202a27a2b8de748200a198f79142cc0c784430af42532d8798f"
         ),
         .binaryTarget(
             name: "BitmovinPlayerAnalytics",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.86.0/BitmovinPlayerAnalytics.zip",
-            checksum: "e0d4b89b4cc5b7aaa5b1677c6b42c007e94fe5fffe59c46424d297d3be42d50f"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.86.1-a.1/BitmovinPlayerAnalytics.zip",
+            checksum: "e38fd7819d9e8ee94994ec25adb55afe1a4a800772faef6d3d5dce83868786a4"
         ),
         .target(
             name: "BitmovinPlayerTarget",
